3670004419eee043a669350bc2ac9a27b7a356ec
tillingspore <joaomariolucena@gmail.com>
2022-11-11 09:39:59 -0300

    rede de pequeno mundo watts-strogatz

---
+++ 2022_2023/watts_strogatz.py
@@ -0,0 +1,24 @@
+import networkx as nx
+import matplotlib.pylab as plt
+
+
+"""
+rede de pequeno mundo
+modelo watts-strogatz
+
+n = numero de nós
+p = ordem e aleatoriedade
+
+
+
+"""
+
+g = nx.watts_strogatz_graph(n=30,k=30,p=.000001)
+
+
+pos = nx.circular_layout(g)
+
+plt.figure(figsize=(12,12))
+nx.draw_networkx(g,pos)
+
+plt.show()
